708db2a76d8c42ad1b788327728b0ee82302be43
alexheidorn <alexheidorn12@gmail.com>
2023-10-10 22:40:11 -0500

    initial commit

---
+++ alexheidorn.cpp
@@ -0,0 +1,20 @@
+/*
+* Alex Heidorn
+* Created: 10-10-23
+* CS2 Lab5
+*  
+*/
+
+#include <iostream>
+#include <fstream> 
+#include <string> 
+#include "alexheidornMyBag_StringLab5.cpp"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
